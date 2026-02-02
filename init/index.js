e415a1248f409b60ca3e329e8895252c382691ff
Anushka Bhosale <bhosaleanushka519@gmail.com>
2026-02-02 23:04:36 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/HomeHaven";
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const iniDB = async () => {
+  await Listing.deleteMany();
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "697b867110e1770c6e4129ae",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+iniDB();
