dde5bdab79e1ce4d9a12354b634bf7472b4b687c
Anushka Bhosale <bhosaleanushka519@gmail.com>
2026-02-08 01:52:36 +0530

    removed cloud atlas database

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require("method-override");
 const ejs = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+//const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -19,7 +19,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/homehaven";
+const Mongo_URL = "mongodb://127.0.0.1:27017/HomeHaven";
 
 main()
   .then(() => {
@@ -30,7 +30,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbUrl);
+  await mongoose.connect(Mongo_URL);
 }
 
 app.set("view engine", "ejs");
@@ -41,20 +41,20 @@ app.use(methodOverride("_method"));
 app.engine("ejs", (engine = ejs));
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
-  touchAfter: 24 * 3600,
-});
+// const store = MongoStore.create({
+//   mongoUrl: dbUrl,
+//   crypto: {
+//     secret: process.env.SECRET,
+//   },
+//   touchAfter: 24 * 3600,
+// });
 
-store.on("error", (err) => {
-  console.log("ERROR in SESSION STORE", err);
-});
+// store.on("error", (err) => {
+//   console.log("ERROR in SESSION STORE", err);
+// });
 
 const sessionOptions = {
-  store,
+  //store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
@@ -102,7 +102,6 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs", { message });
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
